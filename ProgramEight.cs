346c34d512d817b1d01f67a5866297e4724889cb
Izaac Baptista <izaacbaptista@gmail.com>
2021-03-29 19:56:31 -0300

    exe

---
+++ ProgramEight.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Globalization;
+namespace CSharp
+{
+    class ProgramEight
+    {
+        public static void Main(string[] args)
+        {
+            int x, y, z;
+
+            String[] vet = Console.ReadLine().Split(' ');
+
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+            z = int.Parse(vet[2]);
+
+            Console.ReadLine();
+        }
+    }
+}
